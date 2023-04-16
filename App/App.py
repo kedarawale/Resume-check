a08ec6439d8bc9faf63bb65482bcd43ad9b1836b
Kedar <66516089+kedarawale@users.noreply.github.com>
2023-04-16 21:44:39 +0530

    Update App.py

--- App/App.py
+++ App/App.py
@@ -28,7 +28,7 @@ from PIL import Image
 # pre stored data for prediction purposes
 from Courses import ds_course,web_course,android_course,ios_course,uiux_course,resume_videos,interview_videos
 import nltk
-nltk.download('stopwords')
+nltk.download()
 
 
 ###### Preprocessing functions ######
